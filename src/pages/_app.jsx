a53e41dee853f5991063e12108d7aa204c095235
durashere <21038648+durashere@users.noreply.github.com>
2021-08-10 11:45:15 +0200

    refactor: ignore props spreading in _app

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import PropTypes from 'prop-types';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
