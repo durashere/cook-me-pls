c4fa630657fbf143ef29531bf79b0a906f47819d
durashere <21038648+durashere@users.noreply.github.com>
2021-08-16 13:29:33 +0200

    refactor: change env variable name

--- src/backend/mongoose.js
+++ src/backend/mongoose.js
@@ -5,7 +5,7 @@ const dbConnect = (handler) => async (req, res) => {
   if (mongoose.connection.readyState >= 1) {
     return handler(req, res);
   }
-  await mongoose.connect(process.env.MONGODB_URI, {
+  await mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     autoIndex: true,
