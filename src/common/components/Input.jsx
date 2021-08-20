83ee86b3251f9fa69e0bc1c67df5a61d20fea1ca
durashere <21038648+durashere@users.noreply.github.com>
2021-08-20 14:18:25 +0200

    feat: add new input attributes and forwardref

--- src/common/components/Input.jsx
+++ src/common/components/Input.jsx
@@ -1,48 +1,58 @@
+import { forwardRef } from 'react';
 import classNames from 'classnames';
-import React from 'react';
 import PropTypes from 'prop-types';
 
-const Input = ({
-  defaultValue,
-  disabled,
-  fullWidth,
-  label,
-  name,
-  onChange,
-  placeholder,
-  register,
-  required,
-  type,
-  value,
-}) => {
-  const registerProps = register && { ...register(name, { required }) };
+const Input = forwardRef(
+  (
+    {
+      defaultValue,
+      disabled,
+      fullWidth,
+      label,
+      name,
+      onChange,
+      onClick,
+      placeholder,
+      readOnly,
+      register,
+      required,
+      type,
+      value,
+    },
+    ref
+  ) => {
+    const registerProps = register && { ...register(name, { required }) };
 
-  return (
-    <div className={classNames({ 'w-full': fullWidth })}>
-      {label && (
-        <label htmlFor={name}>
-          <span className="capitalize">{label}</span>
-        </label>
-      )}
-      <input
-        className={classNames('block input', {
-          'mt-1': label,
-          'w-full': fullWidth,
-        })}
-        defaultValue={defaultValue}
-        disabled={disabled}
-        name={name}
-        onChange={onChange}
-        placeholder={placeholder}
-        required={required}
-        type={type}
-        value={value}
-        // eslint-disable-next-line react/jsx-props-no-spreading
-        {...registerProps}
-      />
-    </div>
-  );
-};
+    return (
+      <div className={classNames({ 'w-full': fullWidth })}>
+        {label && (
+          <label htmlFor={name}>
+            <span className="capitalize">{label}</span>
+          </label>
+        )}
+        <input
+          className={classNames('block input', {
+            'mt-1': label,
+            'w-full': fullWidth,
+          })}
+          defaultValue={defaultValue}
+          disabled={disabled}
+          name={name}
+          onChange={onChange}
+          onClick={onClick}
+          placeholder={placeholder}
+          readOnly={readOnly}
+          ref={ref}
+          required={required}
+          type={type}
+          value={value}
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          {...registerProps}
+        />
+      </div>
+    );
+  }
+);
 
 Input.propTypes = {
   defaultValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
@@ -51,7 +61,9 @@ Input.propTypes = {
   label: PropTypes.string,
   name: PropTypes.string,
   onChange: PropTypes.func,
+  onClick: PropTypes.func,
   placeholder: PropTypes.string,
+  readOnly: PropTypes.bool,
   register: PropTypes.func,
   required: PropTypes.bool,
   type: PropTypes.string,
@@ -65,7 +77,9 @@ Input.defaultProps = {
   label: undefined,
   name: undefined,
   onChange: undefined,
+  onClick: undefined,
   placeholder: undefined,
+  readOnly: false,
   register: undefined,
   required: false,
   type: 'text',
