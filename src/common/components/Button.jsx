d01ae8a2a88ab0ce27752e455f5a6bab83d3789b
durashere <21038648+durashere@users.noreply.github.com>
2021-08-24 12:06:53 +0200

    refactor: forwardRef button

--- src/common/components/Button.jsx
+++ src/common/components/Button.jsx
@@ -1,31 +1,35 @@
+import { forwardRef } from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
-const Button = ({ children, disabled, fullWidth, htmlType, icon, onClick, type }) => {
-  return (
-    <button
-      className={classNames(
-        'bg-transparent flex items-center justify-center p-2 transition-all rounded-md shadow outline-none focus:outline-none ring-1 hover:ring-2 whitespace-nowrap focus:ring',
-        {
-          'w-full': fullWidth,
-          'text-gray-500 ring-gray-300 hover:ring-gray-400/50 focus:ring-gray-400/50':
-            type === 'default',
-          'text-yellow-500 ring-yellow-500 hover:ring-yellow-500/50 focus:ring-yellow-500/50':
-            type === 'primary',
-          'text-red-500 ring-red-500 hover:ring-red-500/50 focus:ring-red-500/50':
-            type === 'danger',
-        }
-      )}
-      disabled={disabled}
-      onClick={onClick}
-      // eslint-disable-next-line react/button-has-type
-      type={htmlType}
-    >
-      {icon && <span className="material-icons-outlined">{icon}</span>}
-      {children && <span className={classNames('font-medium', { 'ml-2': icon })}>{children}</span>}
-    </button>
-  );
-};
+const Button = forwardRef(
+  ({ children, disabled, fullWidth, htmlType, icon, onClick, type }, ref) => {
+    return (
+      <button
+        className={classNames(
+          'bg-transparent text-gray-500 ring-gray-300 hover:ring-gray-400/50 focus:ring-gray-400/50 flex items-center justify-center p-2 transition-all rounded-md shadow outline-none focus:outline-none ring-1 hover:ring-2 whitespace-nowrap focus:ring',
+          {
+            'w-full': fullWidth,
+            'text-yellow-500 ring-yellow-500 hover:ring-yellow-500/50 focus:ring-yellow-500/50':
+              type === 'primary',
+            'text-red-500 ring-red-500 hover:ring-red-500/50 focus:ring-red-500/50':
+              type === 'danger',
+          }
+        )}
+        disabled={disabled}
+        onClick={onClick}
+        ref={ref}
+        // eslint-disable-next-line react/button-has-type
+        type={htmlType}
+      >
+        {icon && <span className="material-icons-outlined">{icon}</span>}
+        {children && (
+          <span className={classNames('font-medium', { 'ml-2': icon })}>{children}</span>
+        )}
+      </button>
+    );
+  }
+);
 
 Button.propTypes = {
   children: PropTypes.node,
@@ -44,7 +48,7 @@ Button.defaultProps = {
   htmlType: 'button',
   icon: undefined,
   onClick: undefined,
-  type: 'default',
+  type: undefined,
 };
 
 export default Button;
