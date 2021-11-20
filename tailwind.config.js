cffdf5e8cad63eb9559d514dbfc9d51a49fb31d6
durashere <21038648+durashere@users.noreply.github.com>
2021-11-20 09:01:31 +0100

    style: remove animations from filtering

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,18 +7,7 @@ module.exports = {
       poppins: ['Poppins', 'sans-serif'],
       lobster: ['Lobster', 'cursive'],
     },
-    extend: {
-      keyframes: {
-        'puff-in-center': {
-          '0%': { opacity: 0, filter: 'blur(4px)' },
-          '100%': { opacity: 1, filter: 'blur(0px)' },
-        },
-      },
-      animation: {
-        'puff-in-center':
-          'puff-in-center 0.7s cubic-bezier(0.470, 0.000, 0.745, 0.715) both',
-      },
-    },
+    extend: {},
   },
   variants: {
     extend: {},
