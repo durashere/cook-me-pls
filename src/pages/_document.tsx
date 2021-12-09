8418fcc2cfd6128f43a3456a5bdd64cc247e692f
durashere <21038648+durashere@users.noreply.github.com>
2021-12-09 15:14:35 +0100

    feat: add fonts preload

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,27 @@ class CustomDocument extends NextDocument {
       <Html>
         <Head>
           <link rel="icon" href="/chicken.svg" />
+          <link
+            rel="preload"
+            href="/fonts/lobster-v23-latin-400.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/poppins-v15-latin-400.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/poppins-v15-latin-700.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
         </Head>
         <body className="text-gray-700 bg-gray-100 font-poppins">
           <Main />
