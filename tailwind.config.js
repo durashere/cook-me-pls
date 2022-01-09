3b07cc108c9363e0e08bfc16b96a9f7807474ebd
durashere <21038648+durashere@users.noreply.github.com>
2022-01-09 10:59:19 +0100

    chore: move fonts to extend instead replacing them

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,12 @@ const { fontFamily } = require('tailwindcss/defaultTheme');
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    fontFamily: {
-      poppins: ['Poppins', ...fontFamily.sans],
-      lobster: ['Lobster', ...fontFamily.sans],
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', ...fontFamily.sans],
+        lobster: ['Lobster', ...fontFamily.sans],
+      },
     },
-    extend: {},
   },
   plugins: [require('@tailwindcss/forms'), require('@tailwindcss/line-clamp')],
 };
